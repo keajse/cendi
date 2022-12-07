3eba2bd27cdaea36c5d0677872aa758cfb30dbc1
unknown <106707986+MaKeajse@users.noreply.github.com>
2022-12-06 21:41:51 -0500

    Validación de datos nombre y correo
    Evitar que el campo de nombre y correo quedé vacios.

--- Final/app/src/main/java/com/example/afinal/formulario.java
+++ Final/app/src/main/java/com/example/afinal/formulario.java
@@ -3,12 +3,43 @@ package com.example.afinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class formulario extends AppCompatActivity {
+
+    EditText txt_nombre, txt_email, txt_direccion;
+    RadioButton radBtnCompleto, radBtnMedioTiempo;
+    Spinner spnDepartamento;
+    Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        txt_nombre = findViewById(R.id.edt_name);
+        txt_email = findViewById(R.id.edt_email);
+        txt_direccion = findViewById(R.id.edt_Adress);
+        spnDepartamento = findViewById(R.id.spinner);
+
+        radBtnCompleto = findViewById(R.id.rbtn_completo);
+        radBtnMedioTiempo = findViewById(R.id.rbtn_MedioTiempo);
+
+
+    }
+
+    public void guardar(View view){
+
+        if(txt_nombre.getText().toString().equals("") || txt_email.getText().toString().equals("") ){
+            Toast.makeText(formulario.this, "Ingresar nombre y correo", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(formulario.this, "Vamos bien", Toast.LENGTH_LONG);
+        }
+
     }
 }
